e63fc62964584d97ba3d3858f5648adfcdc34eb1
minari-23 <anka.x2002@gmail.com>
2022-11-05 17:32:11 -0400

    Comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 int main() {
+
+// For omniroid
+// We need to initialize read file
+// Read a first line and create 2 data structures
+// First data structure is 2 dimensional with i and j
+// The second one is an array for the portiong of only one value at a time
     return 0
 }
